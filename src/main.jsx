39e01372ca6ea550a4ba23765309f34d22cfc791
Vishalsomaraju <vishalsomaraju9@gmail.com>
2025-06-30 16:05:44 +0530

    deploy

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { HashRouter } from "react-router-dom";
 import { ClerkProvider } from "@clerk/clerk-react";
 import { DataProvider } from "./context/DataContext.jsx";
 import { CartProvider } from "./context/CartContext.jsx";
@@ -25,40 +24,38 @@ root.render(
       publishableKey={PUBLISHABLE_KEY}
       afterSignOutUrl="#/Ecommerce"
     >
-      <HashRouter>
-        <DataProvider>
-          <CartProvider>
-            <App />
-            {/* Scroll To Top Button */}
-            <ScrollToTop
-              smooth
-              color="black"
-              component={<FaArrowUp size={20} />}
-              style={{
-                backgroundColor: "#00FFFF",
-                display: "flex",
-                alignItems: "center",
-                justifyContent: "center",
-                borderRadius: "100%",
-                marginBottom: "-20px",
-              }}
-            />
-            {/* Toast Notification Container */}
-            <ToastContainer
-              position="bottom-right"
-              autoClose={5000}
-              hideProgressBar={false}
-              newestOnTop={false}
-              closeOnClick={true}
-              rtl={false}
-              pauseOnFocusLoss
-              draggable
-              pauseOnHover
-              theme="dark"
-            />
-          </CartProvider>
-        </DataProvider>
-      </HashRouter>
+      <DataProvider>
+        <CartProvider>
+          <App />
+          {/* Scroll To Top Button */}
+          <ScrollToTop
+            smooth
+            color="black"
+            component={<FaArrowUp size={20} />}
+            style={{
+              backgroundColor: "#00FFFF",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              borderRadius: "100%",
+              marginBottom: "-20px",
+            }}
+          />
+          {/* Toast Notification Container */}
+          <ToastContainer
+            position="bottom-right"
+            autoClose={5000}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick={true}
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+            theme="dark"
+          />
+        </CartProvider>
+      </DataProvider>
     </ClerkProvider>
   </StrictMode>
 );
